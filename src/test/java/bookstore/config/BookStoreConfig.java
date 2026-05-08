228102582f7cbd1eae7549ea735d5bcc935138c2
Vladimir <nachev.vlad@gmail.com>
2026-05-08 13:19:34 +0300

    add base url, book endpoints, vers in BookStoreConfig

--- src/test/java/bookstore/config/BookStoreConfig.java
+++ src/test/java/bookstore/config/BookStoreConfig.java
@@ -1,4 +1,21 @@
 package bookstore.config;
 
 public class BookStoreConfig {
+
+    // URLs
+    public static final String BASE_URL = "https://fakerestapi.azurewebsites.net";
+
+    // API versions
+    public static final String API_VERSION = "/api/v1";
+
+    // Endpoints
+    public static final String BOOKS = API_VERSION + "/Books";
+    public static final String BOOK_BY_ID = API_VERSION + "/Books/{id}";
+
+    // Verbs
+    public static final String POST = "POST";
+    public static final String GET = "GET";
+    public static final String PUT = "PUT";
+    public static final String DELETE = "DELETE";
+
 }
